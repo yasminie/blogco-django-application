2916946c675adc99979dda074bb71a1f94a0064d
yasminie <99419128+yasminie@users.noreply.github.com>
2022-08-04 00:08:52 -0400

    Update settings.py

--- cblog/settings.py
+++ cblog/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3aj0mdze%e307cu&fng#oa86a0(hfdi4gt3j0(#c&6n5%-ig7m'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
